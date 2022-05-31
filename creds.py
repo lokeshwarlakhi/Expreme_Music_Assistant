1f488806e9c32b971a5e739cb6e32808d8a853a1
lokeshwarlakhi <lokeshwarlakhi@gmail.com>
2022-05-31 20:55:09 +0530

    2054 31-05-2022

---
+++ creds.py
@@ -0,0 +1,5 @@
+client_id = '4a3052ac28a444a998c36ecf983195d7'
+
+client_secrect = '4b1fa347ab934035bcb8a01bc8ad0efd'
+
+redirect_url = 'http://google.com/'
---
+++ spotify_client_test.py
@@ -0,0 +1,19 @@
+import spotify_client as scl
+
+scl.playsong("Sad")
+
+
+
+# def lol():
+#     print("lokesh")
+# # global cnt ;cnt = 0
+# class lowkey():
+#     def __init__(self):
+#         self.cnt =0
+#     def damn(self):
+#         self.cnt+=1
+#         print(self.cnt)
+
+# p = lowkey()
+# for i in range(2):
+#     p.damn()
